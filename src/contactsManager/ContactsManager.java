3e21de6c4919a1ea40a21d54c13c2cbfb2bce963
Jonathan Clark <jonathan.b.clark1127@gmail.com>
2022-03-10 13:49:29 -0600

    first commit for contacts manager cli

---
+++ src/contactsManager/ContactsManager.java
@@ -0,0 +1,181 @@
+package contactsManager;
+
+import util.Input;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class ContactsManager {
+    public static String headerRow;
+    public static ArrayList<String> names = new ArrayList<>();
+    public static ArrayList<String> numbers = new ArrayList<>();
+    public static Input sc = new Input();
+
+
+    public static void main(String[] args) { // main
+        try {
+            readFiles();
+            runProgram();
+            writeFiles();
+        } catch (Exception e) {
+            System.out.println("Something went wrong.");
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void readFiles() throws ArrayIndexOutOfBoundsException { //reads data (if any) from text file and populates list
+        Path file = Paths.get("src/contactsManager/contacts.txt");
+        Charset charset = StandardCharsets.US_ASCII;
+        try (BufferedReader reader = Files.newBufferedReader(file, charset)) {
+            String line;
+            headerRow = reader.readLine();
+            while ((line = reader.readLine()) != null) {
+                String[] values = line.split(",");
+                names.add(values[0]);
+                numbers.add(values[1]);
+            }
+        } catch (IOException x) {
+            System.err.format("IOException: %s%n", x);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(" Warning!!! File you are trying to access has no content.");
+        }
+    }
+
+    public static void runProgram() { //gets user entry for main menu prompt and executes corresponding function
+        boolean keepGoing = true;
+        while (keepGoing) {
+            mainMenu();
+            String entry = sc.getString("Please select one of the following.");
+            switch (entry) {
+                case "1":
+                    viewContacts();
+                    break;
+                case "2":
+                    addEntry();
+                    break;
+                case "3":
+                    searchEntries();
+                    break;
+                case "4":
+                    deleteEntry();
+                    break;
+                case "5":
+                    keepGoing = false;
+                    System.out.println("Program Exiting...");
+                    break;
+                default:
+                    System.out.println("That is not one of the choices.");
+
+            }
+        }
+    }
+
+    public static void mainMenu() {  //prompt for main menu
+        System.out.println();
+        System.out.println("--------------- Contacts Manager ---------------");
+        System.out.println("1. View Contacts");
+        System.out.println("2. Add a new Contact.");
+        System.out.println("3. Search for a contact by name.");
+        System.out.println("4. Delete an existing contact.");
+        System.out.println("5. Exit");
+        System.out.println();
+    }
+
+    public static void viewContacts() throws IndexOutOfBoundsException { //lists all contacts in phonebook
+        try {
+            System.out.println();
+            System.out.printf("Name     \t | \t\tPhone number%n");
+            System.out.println("--------------------------------");
+            for (int i = 0; i < names.size(); i++) {
+
+                System.out.printf("%s     \t | \t\t%s %n", names.get(i), numbers.get(i));
+            }
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("There are no contacts to display.");
+        }
+    }
+
+    public static void addEntry() { // adds an entry in the phone book
+        String name;
+        String number;
+        name = sc.getString("Enter entry name.");
+        if (names.contains(name.toLowerCase())) {
+            boolean overwrite = sc.yesNo("There is already an entry for " + name + ", would you like to overwrite entry? y/n?");
+            if (overwrite) {
+                number = sc.getString("Enter phone number for " + name + ".");
+                int i = names.indexOf(name.toLowerCase());
+                numbers.remove(i);
+                numbers.add(i, phoneNumberFormatter(number));
+                System.out.println("Contact updated.");
+            } else {
+                addEntry();
+            }
+        } else {
+            number = sc.getString("Enter entry phone number.");
+            names.add(name);
+            numbers.add(phoneNumberFormatter(number));
+        }
+    }
+
+    public static void searchEntries() { // searches entries in phonebook by name
+        System.out.println();
+        String query = sc.getString("Enter the name you would like to look for.").toLowerCase();
+        int i = names.indexOf(query.toLowerCase());
+        System.out.println();
+        if (i != -1) {
+            System.out.println();
+            System.out.println(names.get(i) + " -- " + numbers.get(i));
+            System.out.println();
+        } else {
+            System.out.println("Contact not found.");
+            System.out.println();
+        }
+    }
+
+    public static void deleteEntry() throws IndexOutOfBoundsException { //removes entries by name
+        System.out.println();
+        for (String name : names) {
+            System.out.print(name + " ");
+        }
+        System.out.println();
+        String query = sc.getString("Enter the name of the entry you would like to delete.");
+        try {
+            int i = names.indexOf(query);
+            names.remove(i);
+            numbers.remove(i);
+            System.out.println(query + " has been removed from your contacts.");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("There are no contacts to modify.");
+        }
+    }
+
+    public static void writeFiles() { // saves entries in phonebook to .txt file
+        Path file = Paths.get("src/contactsManager/contacts.txt");
+        Charset charset = StandardCharsets.US_ASCII;
+        String line;
+        try (BufferedWriter writer = Files.newBufferedWriter(file, charset)) {
+            writer.write(headerRow + "\n");
+            for (int i = 0; i < names.size(); i++) {
+                line = String.format("%s,%s%n", names.get(i), numbers.get(i));
+                writer.write(line);
+            }
+        } catch (IOException x) {
+            System.err.format("IOException: %s%n", x);
+        }
+    }
+
+    public static String phoneNumberFormatter(String number) { // adds a dash to phone number
+        if (number.length() == 7) {
+            number = number.replaceFirst("((\\d{3})(\\d+))", "$2-$3");
+        } else if (number.length() == 10) {
+            number = number.replaceFirst("(\\d{3})(\\d{3})(\\d+)", "$1-$2-$3");
+        }
+        return number;
+    }
+}
---
+++ src/util/Input.java
@@ -0,0 +1,95 @@
+package util;
+
+import java.util.Scanner;
+
+public class Input {
+    private Scanner sc = new Scanner(System.in);
+
+    public String getString(String prompt) {
+        System.out.println(prompt);
+        String str = sc.nextLine();
+        return str;
+    }
+
+    public boolean yesNo(String prompt) {
+        boolean trueFalse = true;
+        boolean keepGoing = true;
+        while (keepGoing) {
+            System.out.println(prompt);
+            String yorn = sc.nextLine();
+            if (yorn.equalsIgnoreCase("y")) {
+                keepGoing = false;
+            } else if (yorn.equalsIgnoreCase("n")) {
+                trueFalse = false;
+                keepGoing = trueFalse;
+            } else {
+                System.out.println("Please enter a valid response.");
+            }
+        }
+        return trueFalse;
+    }
+
+    public int getInt(int min, int max, String prompt) {
+        boolean keepGoing = true;
+        int n1 = 0;
+        while (keepGoing) {
+            System.out.println(prompt);
+            n1 = Integer.parseInt(sc.nextLine());
+            if (n1 > max || n1 < min) {
+                System.out.println("Please enter a valid choice");
+            } else {
+                keepGoing = false;
+            }
+        }
+        return n1;
+    }
+
+    public int getInt(String prompt) {
+        System.out.println(prompt);
+        int n1 = 0;
+        try {
+            n1 = Integer.valueOf(getString(prompt));
+        } catch (NumberFormatException e) {
+            System.out.println("Not a valid input");
+        }
+        return n1;
+    }
+
+    public double getDouble(double min, double max, String prompt) {
+        boolean keepGoing = true;
+        double n1 = 0.00;
+        while (keepGoing) {
+            System.out.println(prompt);
+            n1 = Double.parseDouble(sc.nextLine());
+            if (n1 > max || n1 < min) {
+                System.out.println("Please enter a valid choice");
+            } else {
+                keepGoing = false;
+            }
+        }
+        return n1;
+    }
+
+    public double getDouble(String prompt) {
+//        sc.nextLine();
+        System.out.println(prompt);
+        double n1 = 0.00;
+        try {
+            n1 = Double.valueOf(getString(prompt));
+        } catch (NumberFormatException e) {
+            System.out.println("Not a valid input.");
+        }
+        return n1;
+    }
+
+    public static void main(String[] args) {
+        Input sc = new Input();
+        System.out.println(sc.getString("Please enter a string."));
+        System.out.println(sc.yesNo("Enter y or n."));
+        System.out.println(sc.getInt("Enter an integer."));
+        System.out.println(sc.getInt(1, 10, "Enter an integer between 1 and 10."));
+        System.out.println(sc.getDouble("Enter a Double."));
+        System.out.println(sc.getDouble(1.0, 10.0, "Enter a double between 1.0 and 10.0"));
+    }
+
+}
